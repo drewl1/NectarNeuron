eae19ada5dc131b24868b01cd9c60cba3b9fb311
drewl1 <drewlacoss@gmail.com>
2024-03-29 02:53:47 -0500

    update exp_sys.py

--- cogs/exp_sys.py
+++ cogs/exp_sys.py
@@ -24,58 +24,84 @@ class MongoDBCog(commands.Cog):
         if message.author.bot:
             return
         if isinstance(message.channel, discord.TextChannel):
-            user_id = str(message.author.id)
-            user_data = self.collection.find_one({"user_id": user_id})
-            if user_data:
-                exp = user_data.get("exp", 0)
-                needed_exp = user_data.get("neededExp", 100)
-                level = user_data.get("level", 1)
-                new_exp = exp + 10
-                if new_exp >= needed_exp:
-                    level += 1
-                    new_needed_exp = 100 * (level ** 2) if level != 1 else 200
+            if len(message.content) > 25:
+                user_id = str(message.author.id)
+                user_data = self.collection.find_one({"user_id": user_id})
+                if user_data:
+                    exp = user_data.get("exp", 0)
+                    needed_exp = user_data.get("neededExp", 100)
+                    level = user_data.get("level", 1)
+                    new_exp = exp + 10
+                    if new_exp >= needed_exp:
+                        level += 1
+                        new_needed_exp = 100 * (level ** 2) if level != 1 else 200
 
-                    embed = discord.Embed(title="🎉 LEVEL UP", url="https://example.com",description=f'{message.author.mention} has reached level {level}!\nThats {new_exp} exp!',colour=0xfbff00, timestamp=datetime.now())
-                    embed.set_footer(text="Nectar Neuron")
-                    await message.channel.send(embed=embed)
+                        embed = discord.Embed(title="🎉 LEVEL UP", url="https://example.com",description=f'{message.author.mention} has reached level {level}!\nThats {new_exp} exp!',colour=0xfbff00, timestamp=datetime.now())
+                        embed.set_footer(text="Nectar Neuron")
+                        await message.channel.send(embed=embed)
 
-                    self.collection.update_one(
-                        {"user_id": user_id},
-                        {"$set": {"exp": new_exp, "level": level, "neededExp": new_needed_exp}}
-                    )
+                        self.collection.update_one(
+                            {"user_id": user_id},
+                            {"$set": {"exp": new_exp, "level": level, "neededExp": new_needed_exp}}
+                        )
+                    else:
+                        self.collection.update_one(
+                            {"user_id": user_id},
+                            {"$set": {"exp": new_exp}}
+                        )
                 else:
-                    self.collection.update_one(
-                        {"user_id": user_id},
-                        {"$set": {"exp": new_exp}}
-                    )
-            else:
-                new_user_data = {
-                    "user_id": user_id,
-                    "username": str(message.author),
-                    "level": 1,
-                    "exp": 10,
-                    "neededExp": 100
-                }
-                self.collection.insert_one(new_user_data)
+                    new_user_data = {
+                        "user_id": user_id,
+                        "username": str(message.author),
+                        "level": 1,
+                        "exp": 10,
+                        "neededExp": 100
+                    }
+                    self.collection.insert_one(new_user_data)
+
+
+    @commands.command(aliases=['lb'])
+    async def leaderboard(self, ctx):
+        leaderboard_data = self.collection.find().sort("exp", pymongo.DESCENDING).limit(10)
+        embed = discord.Embed(title="Leaderboard", color=0xffd700, timestamp=datetime.now())
+        for idx, user in enumerate(leaderboard_data, start=1):
+            user_id = user["user_id"]
+            exp = user.get("exp", 0)
+            level = user.get("level", 1)
+            username = user.get("username", "Unknown User")
+            embed.add_field(name=f"{idx}. {username} - lvl{level}", value=f"{exp} EXP", inline=False)
+        embed.set_footer(text="Nectar Neuron")
+        await ctx.send(embed=embed)
+
 
 
     @commands.command()
-    async def level(self, ctx):
-        user_id = str(ctx.author.id)
-        user_data = self.collection.find_one({"user_id": user_id})
+    async def level(self, ctx, *, username: str = None):
+        if username is None:
+            user_id = str(ctx.author.id)
+            user_data = self.collection.find_one({"user_id": user_id})
+            username = ctx.author.display_name
+        else:
+            user_data = self.collection.find_one({"username": username})
 
         if user_data:
             exp = user_data.get("exp", 0)
             level = user_data.get("level", 1)
             needed_exp = user_data.get("neededExp", 100)
             messages_needed = max(0, needed_exp - exp) / 10
-            
-            embed = discord.Embed(title=ctx.author.display_name,description=f"**Level:** {level}\n**Experience:** {exp}\n**Messages to Level Up:** {str(messages_needed)[:-2].strip()}",colour=0x98c379, timestamp=datetime.now())
-            embed.set_thumbnail(url=ctx.author.avatar.url if ctx.author.avatar else discord.Embed.Empty)
+            member = ctx.guild.get_member(int(user_data["user_id"]))
+            thumbnail_url = member.avatar.url if member and member.avatar else None
+            embed = discord.Embed(title=username, description=f"**Level:** {level}\n**Experience:** {exp}\n**Messages to Level Up:** {str(messages_needed)[:-2].strip()}", color=0x98c379, timestamp=datetime.now())
+            if thumbnail_url:
+                embed.set_thumbnail(url=thumbnail_url)
             embed.set_footer(text="Nectar Neuron")
             await ctx.send(embed=embed)
-            
-            
-            
+        else:
+            await ctx.send("User not found in the database.")
+
+
+
+
+        
 async def setup(bot):
     await bot.add_cog(MongoDBCog(bot))
